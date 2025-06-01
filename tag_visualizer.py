5460fb96572d0f36617915e8fa99788cf86c13ae
Hankie1232 <beraquithankie@gmail.com>
2025-06-01 23:59:15 +0800

    changed the floor extent

--- tag_visualizer.py
+++ tag_visualizer.py
@@ -92,6 +92,7 @@ num_points = st.selectbox("Show how many latest positions?", [1, 5, 20, 50, 100,
 # Plot setup
 fig, ax = plt.subplots()
 # Plot the background image
+bg_img = np.fliplr(bg_img)
 ax.imshow(bg_img, extent=extent, origin="lower", zorder=0)
 ax.set_xlim(extent[0], extent[1])
 ax.set_ylim(extent[2], extent[3])
