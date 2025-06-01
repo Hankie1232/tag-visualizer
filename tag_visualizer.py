7a97da55234a2f489c9f913c01fd0f1ee1c465ac
Hankie1232 <beraquithankie@gmail.com>
2025-06-01 22:51:01 +0800

    changed lower to upper

--- tag_visualizer.py
+++ tag_visualizer.py
@@ -58,7 +58,7 @@ num_points = st.selectbox("Show how many latest positions?", [1, 5, 20, 50, 100,
 
 # Plot setup
 fig, ax = plt.subplots()
-ax.imshow(bg_img, extent=extent, origin='lower', zorder=0)
+ax.imshow(bg_img, extent=extent, origin='upper', zorder=0)
 ax.set_xlim(extent[0], extent[1])
 ax.set_ylim(extent[2], extent[3])
 ax.set_title(f"{floor.upper()} - Tag Positions")
